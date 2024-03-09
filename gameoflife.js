c5edc8c508f15abce2d7fbc463e106aa646905d1
HakuTheDeadBoi <muffitoftea@outlook.cz>
2024-03-09 01:59:48 +0100

    ToDo list moved from gameoflife.js script into md file

--- gameoflife.js
+++ gameoflife.js
@@ -1,21 +1,3 @@
-/*
-    GAME OF LIFE
-
-    to do:
-    - allow user to modify speed
-        probably by mouse wheel or clicking
-    - allow to modify rules
-    - allow to stop game or pause
-    - allow to define number of rows/cols
-    - allow to manually change pattern
-        when activated, stop game and allow user
-        to revert black into white and vice versa
-        by clicking on cell area
-     
-    - separate drawing in module to be able use
-        more implementations
-*/
-
 function getMatrix(rows, cols) {
     let newMatrix = new Array(rows);
 
